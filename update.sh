46f542ffabe93382bc2e121f9c1093c364981fa9
numberwolf <porschegt23@foxmail.com>
2016-12-18 17:19:09 +0800

    up

---
+++ update.sh
@@ -0,0 +1,9 @@
+git add -A;
+#git commit -m $(date +%Y%m%d);
+#git push -u origin master;
+read -p "此次上传:"  val
+##echo $val
+git commit -m $val
+# git push all --all
+git push origin master
+#ssh -p 22 root@numberwolf.top 'cd /var/www/html/ME/ && git reset --hard && git pull origin master && chmod -R 777 ../ME/ && cd ../../ && ./write_me_db.sh'
