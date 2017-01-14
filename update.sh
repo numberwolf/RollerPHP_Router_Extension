a17812d7b4f3458972f14768f91cbf99605f6aa0
numberwolf <porschegt23@foxmail.com>
2017-01-14 13:31:09 +0800

    update,date:20170114

--- update.sh
+++ update.sh
@@ -1,9 +1,37 @@
-git add -A;
+#git add -A;
 #git commit -m $(date +%Y%m%d);
 #git push -u origin master;
-read -p "此次上传:"  val
+#read -p "此次上传:"  val
 ##echo $val
-git commit -m $val
-git push origin --all
+#git commit -m $val
+#git push origin --all
 #git push origin master
-#ssh -p 22 root@numberwolf.top 'cd /var/www/html/ME/ && git reset --hard && git pull origin master && chmod -R 777 ../ME/ && cd ../../ && ./write_me_db.sh'
+
+COMMIT_VAL=""
+DATE=$(date +%Y%m%d)
+
+function github {
+	mv .git/config.github.com .git/config
+	git add -A
+	read -p "commit:" val
+	COMMIT_VAL=${val}",date:"${DATE}
+	git commit -m ${COMMIT_VAL}
+	git push origin master
+	mv .git/config .git/config.github.com
+}
+
+function coding {
+	mv .git/config.coding.net .git/config
+	git add -A
+	git commit -m ${COMMIT_VAL}
+	git push origin master
+	mv .git/config .git/config.coding.net
+}
+
+function main {
+	github
+	coding
+}
+
+main
+
